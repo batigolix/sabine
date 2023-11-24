80fa4ec5b1212fc870a3051d5f69ad04a455f592
Boris Doesborg <boris.doesborg@finalist.nl>
2023-11-24 15:16:12 +0100

    Site installation & more

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -780,3 +780,12 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+if (strpos(DRUPAL_ROOT, 'production')) {
+  include '/home/doesborg/public_html/sabine-project/settings/production.php';
+}
+
+if (strpos(DRUPAL_ROOT, 'stage')) {
+  include '/home/doesborg/public_html/sabine-project/settings/stage.php';
+}
+
