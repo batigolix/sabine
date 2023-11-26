0aa5ebf6566de51c7f97cbb4ff4fa2f9fb20ccf3
Boris Doesborg <boris.doesborg@finalist.nl>
2023-11-26 04:50:58 +0100

    Site installation & more

--- web/modules/custom/sabine_tools/src/Form/ContactForm.php
+++ web/modules/custom/sabine_tools/src/Form/ContactForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\doesdesign_tools\Form;
+namespace Drupal\sabine_tools\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -54,11 +54,6 @@ class ContactForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['contact'] = [
-      '#markup' => $this->t('Gebruik het onderstaande contactformulier voor vragen of opmerkingen'),
-      '#prefix' => '<div class="fields"><div class="field">',
-      '#suffix' => '</div>',
-    ];
     $form['name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Naam'),
@@ -66,41 +61,54 @@ class ContactForm extends FormBase {
       '#size' => 64,
       '#weight' => '0',
       '#required' => TRUE,
+      '#prefix' => '<div class="col-12">',
+      '#suffix' => '</div>',
     ];
     $form['email'] = [
       '#type' => 'email',
       '#title' => $this->t('E-mailadres'),
       '#weight' => '0',
       '#required' => TRUE,
+      '#prefix' => '<div class="col-6 col-12-mobile">',
+      '#suffix' => '</div>',
     ];
     $form['telephone'] = [
       '#type' => 'tel',
       '#title' => $this->t('Telefoonnummer'),
       '#weight' => '0',
       '#required' => TRUE,
+      '#prefix' => '<div class="col-6 col-12-mobile">',
+      '#suffix' => '</div>',
     ];
     $form['subject'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Onderwerp'),
       '#weight' => '0',
       '#required' => TRUE,
+      '#prefix' => '<div class="col-12">',
+      '#suffix' => '</div>',
     ];
     $form['message'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Vraag of opmerking'),
       '#weight' => '0',
-      '#suffix' => '</div>',
       '#required' => TRUE,
+      '#prefix' => '<div class="col-12">',
+      '#suffix' => '</div>',
     ];
     $form['tools'] = [
       '#type' => 'textfield',
       '#title' => t('Noem drie letters uit de naam van de site'),
       '#size' => 24,
       '#description' => t('Ik stel deze vraag om te controleren dat dit formulier niet door een robot wordt ingevuld'),
+      '#prefix' => '<div class="col-12">',
+      '#suffix' => '</div>',
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Versturen'),
+      '#prefix' => '<div class="col-12 centered">',
+      '#suffix' => '</div>',
     ];
     return $form;
   }
@@ -111,11 +119,11 @@ class ContactForm extends FormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $tools_string = strtolower($form_state->getValue('tools'));
     $letters = str_split($tools_string);
-    $doesdesign_letters = str_split('doesdesign.nl');
-    $result = array_intersect($letters, $doesdesign_letters);
+    $sabine_letters = str_split('sabine.nl');
+    $result = array_intersect($letters, $sabine_letters);
     $result = count($result);
     if ($result < 3) {
-      $form_state->setError($form, t('Het antwoord is niet juist. Om te controleren of u geen spamrobot bent, vraag ik om 3 letters uit de naam van de site in te voeren. Als het niet lukt, stuur dan een email naar birgit@doesdesign.nl'));
+      $form_state->setError($form, t('Het antwoord is niet juist. Om te controleren of u geen spamrobot bent, vraag ik om 3 letters uit de naam van de site in te voeren. Als het niet lukt, stuur dan een email naar bdoesborg@gamil.com'));
     }
     parent::validateForm($form, $form_state);
   }
@@ -129,16 +137,17 @@ class ContactForm extends FormBase {
     $message = $form_state->getValue('message');
     $subject = $form_state->getValue('subject');
     $mailManager = \Drupal::service('plugin.manager.mail');
-    $module = 'doesdesign_tools';
-    $key = 'doesdesign_tools_mail';
-    $to = "coxdoes@gmail.com";
+    $module = 'sabine_tools';
+    $key = 'sabine_tools_mail';
+    $to = "bdoesborg@gmail.com";
     $params['message'] = "Onderwerp: $subject \n\nNaam: $name \n\nTelefoon: $telephone\n\nBericht: $message";
     $params['subject'] = $subject;
     $langcode = \Drupal::currentUser()->getPreferredLangcode();
     $send = TRUE;
     $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
     if ($result['result'] !== TRUE) {
-      $this->messenger()->addError(t('There was a problem sending your message and it was not sent.'));
+      $this->messenger()
+        ->addError(t('There was a problem sending your message and it was not sent.'));
     }
     else {
       $this->messenger()->addStatus(t('Your message has been sent.'));
