81546865a1f1969fba485ed8e2af11edd745b87d
Boris Doesborg <boris.doesborg@finalist.nl>
2024-01-20 12:44:57 +0100

    Site installation & more

--- web/themes/custom/prologue/assets/js/main.js
+++ web/themes/custom/prologue/assets/js/main.js
@@ -29,73 +29,73 @@
 	// Nav.
 		var $nav_a = $nav.find('a');
 
-		$nav_a
-			.addClass('scrolly')
-			.on('click', function(e) {
-
-				var $this = $(this);
-
-				// External link? Bail.
-					if ($this.attr('href').charAt(0) != '#')
-						return;
-
-				// Prevent default.
-					e.preventDefault();
-
-				// Deactivate all links.
-					$nav_a.removeClass('active');
-
-				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-					$this
-						.addClass('active')
-						.addClass('active-locked');
-
-			})
-			.each(function() {
-
-				var	$this = $(this),
-					id = $this.attr('href'),
-					$section = $(id);
-
-				// No section for this link? Bail.
-					if ($section.length < 1)
-						return;
-
-				// Scrollex.
-					$section.scrollex({
-						mode: 'middle',
-						top: '-10vh',
-						bottom: '-10vh',
-						initialize: function() {
-
-							// Deactivate section.
-								$section.addClass('inactive');
-
-						},
-						enter: function() {
-
-							// Activate section.
-								$section.removeClass('inactive');
-
-							// No locked links? Deactivate all links and activate this section's one.
-								if ($nav_a.filter('.active-locked').length == 0) {
-
-									$nav_a.removeClass('active');
-									$this.addClass('active');
-
-								}
-
-							// Otherwise, if this section's link is the one that's locked, unlock it.
-								else if ($this.hasClass('active-locked'))
-									$this.removeClass('active-locked');
-
-						}
-					});
-
-			});
-
-	// Scrolly.
-		$('.scrolly').scrolly();
+	// 	$nav_a
+	// 		.addClass('scrolly')
+	// 		.on('click', function(e) {
+  //
+	// 			var $this = $(this);
+  //
+	// 			// External link? Bail.
+	// 				if ($this.attr('href').charAt(0) != '#')
+	// 					return;
+  //
+	// 			// Prevent default.
+	// 				e.preventDefault();
+  //
+	// 			// Deactivate all links.
+	// 				$nav_a.removeClass('active');
+  //
+	// 			// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+	// 				$this
+	// 					.addClass('active')
+	// 					.addClass('active-locked');
+  //
+	// 		})
+	// 		.each(function() {
+  //
+	// 			var	$this = $(this),
+	// 				id = $this.attr('href'),
+	// 				$section = $(id);
+  //
+	// 			// No section for this link? Bail.
+	// 				if ($section.length < 1)
+	// 					return;
+  //
+	// 			// Scrollex.
+	// 				$section.scrollex({
+	// 					mode: 'middle',
+	// 					top: '-10vh',
+	// 					bottom: '-10vh',
+	// 					initialize: function() {
+  //
+	// 						// Deactivate section.
+	// 							$section.addClass('inactive');
+  //
+	// 					},
+	// 					enter: function() {
+  //
+	// 						// Activate section.
+	// 							$section.removeClass('inactive');
+  //
+	// 						// No locked links? Deactivate all links and activate this section's one.
+	// 							if ($nav_a.filter('.active-locked').length == 0) {
+  //
+	// 								$nav_a.removeClass('active');
+	// 								$this.addClass('active');
+  //
+	// 							}
+  //
+	// 						// Otherwise, if this section's link is the one that's locked, unlock it.
+	// 							else if ($this.hasClass('active-locked'))
+	// 								$this.removeClass('active-locked');
+  //
+	// 					}
+	// 				});
+  //
+	// 		});
+  //
+	// // Scrolly.
+	// 	$('.scrolly').scrolly();
 
 	// Header (narrower + mobile).
 
